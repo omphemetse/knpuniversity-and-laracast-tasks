32ad146890278277a8d6fca6af76cdc91fe75522
Samuel Mafoko <samuelm@miway.co.za>
2017-10-19 20:20:07 +0200

    Add GET HTTP Verb to dictate the method used to acquire data. Add Json Response to a method used for the API call.

--- knpuniversity-tasks/symfony/aqua_note/src/AppBundle/Controller/GenusController.php
+++ knpuniversity-tasks/symfony/aqua_note/src/AppBundle/Controller/GenusController.php
@@ -8,9 +8,11 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class GenusController extends Controller
 {
@@ -25,11 +27,30 @@ class GenusController extends Controller
             'Inked!'
         ];
 
-        $html = $this->render('genus/show.html.twig',[
+        $html = $this->render('genus/show.html.twig', [
             'name' => $genusName,
             'notes' => $notes
         ]);
 
         return new Response($html);
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes", name="genus_show_notes")
+     * @Method("GET")
+     */
+    public function getNotesAction($genusName)
+    {
+        $notes = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'notes' => $notes
+        ];
+
+        return new JsonResponse($data);
     }
 }
